b2825c483abde64ac5561f3ae660c6d664fd76ec
Maik Glatki <maik@glatki.eu>
2018-01-30 19:46:29 +0000

    Some working version.

--- solve.py
+++ solve.py
@@ -1,21 +1,54 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
+import logging
+
+logging.basicConfig()
+logger = logging.getLogger('rssolver')
+logger.setLevel(logging.INFO)
+#logger.setLevel(logging.DEBUG)
 appointments = [
     {
-        "_comment": "Korrosion kermaischer Werkstoffe",
-        "monday" : [range(830, 1530)],
+        "name": "Korrosion keramischer Werkstoffe",
+        "monday" : [range(830, 1130)],
         "wednesday" : [range(1815, 1930)]
     },
     {
-        "_comment" : "Kommunikative Usability",
+        "name" : "Kommunikative Usability - Übung Montag",
+        "monday" : [range(1215, 1530)],
+    },
+    {
+        "name" : "Kommunikative Usability - Übung Donnerstag",
         "monday" : [range(1215, 1330)],
         "thursday" : [range(1015, 1130)]
     },
     {
-        "_comment" : "Werkstoffkunde der hochtemp",
+        "name" : "Werkstoffkunde der Hochtemperatur",
         "monday" : [range(1500, 1745)],
         "tuesday" : [range(1615, 1730)],
         "friday" : [range(1000, 1115)],
+    },
+    {
+        "name" : "Feuerfeste Werkstoffe",
+        "monday" : [range(830, 945),range(1615,1730)],
+        "tuesday": [range(830, 945)],
+        "friday" : [range(1215, 1330)],
+    },
+    {
+        "name" : "Technologie der Gusswerkstoffe",
+        "tuesday": [range(1315, 1430)],
+        "wednesday" : [range(800, 1145)],
+    },
+    {
+        "name" : "Thermochemie mineralischer Werkstoffe",
+        "tuesday" : [range(830, 1115), range(1215,1330)],
+        "wednesday" : [range(1030, 1145)],
+        "thursday" : [range(1000, 1115)],
+    },
+    {
+        "name" : "Berechnung und Auslegung Industrieofen",
+        "wednesday" : [range(830, 945)],
+        "friday" : [range(1015, 1130)],
     }
 ]
 
@@ -24,31 +57,39 @@ results = []
 for a in range(len(appointments)):
     for b in range(a + 1, len(appointments)):
         for c in range(b + 1, len(appointments)):
-            print("plan: %s, %s, %s" %(a,b,c))
+            logger.debug("plan: %s, %s, %s" %(a,b,c))
             if ( a == b or b == c or c == a ):
-                print("skip")
+                logger.debug("skip")
                 break
             # try to place
             no_fit = False
             days = ["monday", "tuesday", "wednesday", "thursday", "friday"]
             plan = {}
             for day in days:
-                print("%s: fit '%s'" % (day, appointments[a]["_comment"]))
                 plan[day] = []
                 for app in [a, b, c]:
-                    for item in appointments[app][day]:
-                        xs = set(plan[day])
-                        if not len(xs.intersection(appointments[app][day])):
-                            no_fit = True
-                            break
-                        plan[day] += appointments[app][day]
+                    try:
+                        logger.debug("  %s: fit '%s'" % (day, appointments[app]["name"]))
+                        for item in appointments[app][day]:
+                            if no_fit:
+                                break
+                            xs = set(plan[day])
+                            if len(xs.intersection(item)) is not 0:
+                                logger.debug("    found appointment overlap")
+                                no_fit = True
+                                break
+                            plan[day] += item
+                    except:
+                        pass # free time
                 if no_fit:
                     break
             if no_fit:
-                print("plan invalid")
+                logger.debug("  plan invalid")
                 break
             # if we arrived here, no collisions found
-            print("Found valid plan")
+            logger.info("Found valid plan")
+            for app in [a,b,c]:
+                logger.info("  %s" % appointments[app]['name'])
             results += plan
 
 
